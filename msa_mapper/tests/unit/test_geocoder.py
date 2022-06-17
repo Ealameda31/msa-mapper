3f4fa1a029cac132865532b3b52a5867348740d4
QuiCorn Alameda-Geiken <emalameda96@gmail.com>
2022-06-17 02:21:10 -0500

    Added correct error handling geocoder test

--- msa_mapper/tests/unit/test_geocoder.py
+++ msa_mapper/tests/unit/test_geocoder.py
@@ -8,13 +8,13 @@ def test_sample():
     """
     addrss, coords = get_geoinfo('801 Grand Ave, Des Moines, Iowa, 50309')
 
-    if not isinstance(addrss, str):
-        raise TypeError('Address returned by geocoder is not a string')
-    if not (isinstance(coords, tuple) and len(coords) == 2):
-        raise ValueError('Coordinates were not returned as a two-length tuple')
+    assert isinstance(addrss, str),  \
+        AssertionError('Address returned by geocoder is not a string')
+    assert (isinstance(coords, tuple) and len(coords) == 2), \
+        AssertionError('Coordinates were not returned as a two-length tuple')
 
-    if addrss.strip() != '801 Grand Ave, Des Moines, Iowa, 50309':
-        raise ValueError('Test case address was not expected output')
+    assert addrss.strip() == '801 Grand Ave, Des Moines, Iowa, 50309', \
+        ValueError('Test case address was not expected output')
     assert (isclose(coords[0], -93.63, abs_tol=0.01)
             and isclose(coords[1], 41.59, abs_tol=0.01)), \
-        "Test case coordinates did not match expected values"
+        ValueError("Test case coordinates did not match expected values")
